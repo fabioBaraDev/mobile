3c8612d706eb94be980dcce9067ffdb92f10a2e6
Logon Aluno <logonaluno@local.com>
2019-12-14 11:19:16 -0300

    primeiro commit do projeto de aula

---
+++ CalculadoraFlex/app/src/main/java/com/example/calculadoraflex/ui/form/FormActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calculadoraflex.ui.form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculadoraflex.R
+
+class FormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form)
+    }
+}
---
+++ CalculadoraFlex/app/src/main/java/com/example/calculadoraflex/ui/result/ResultActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calculadoraflex.ui.result
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculadoraflex.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
